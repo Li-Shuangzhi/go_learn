dae1629df30b2e0e427d8c876b8bacc683646496
lishuangzhi <2930918598@qq.com>
2021-08-23 13:49:34 +0800

    organize the project structure add new go file

---
+++ algorithm/leetcode/august/late/day0823/test.go
@@ -0,0 +1,47 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+
+}
+
+func trimBST(root *TreeNode, low int, high int) *TreeNode {
+	root = trimBSTLeft(root, low)
+	return trimBSTRight(root, high)
+}
+func trimBSTLeft(root *TreeNode, low int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val > low {
+		root.Left = trimBSTLeft(root.Left, low)
+		return root
+	}
+	if root.Val < low {
+		root.Left = nil
+		return trimBSTLeft(root.Right, low)
+	}
+	root.Left = nil
+	return root
+}
+
+func trimBSTRight(root *TreeNode, high int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val < high {
+		root.Right = trimBSTRight(root.Right, high)
+		return root
+	}
+	if root.Val > high {
+		root.Right = nil
+		return trimBSTRight(root.Left, high)
+	}
+	root.Right = nil
+	return root
+}
