8273a9857e838d0119a93f7b113a670aae3cd47c
lishuangzhi <2930918598@qq.com>
2021-08-29 13:58:57 +0800

    unionfind学习

---
+++ algorithm/leetcode/august/late/day0827/union-find.go
@@ -0,0 +1,36 @@
+package main
+
+//
+//import "fmt"
+//
+//var parent []int
+//
+//func main() {
+//	initFindUnion(9)
+//	fmt.Println(isConnected(1, 5))
+//	union(1, 5)
+//	union(1,7)
+//	fmt.Println(isConnected(1, 7))
+//	fmt.Println(parent)
+//}
+//
+//func union(x, y int) {
+//	p := parent[x]
+//	q := parent[y]
+//	for i := 0; i < len(parent); i++ {
+//		if parent[i] == p {
+//			parent[i] = q
+//		}
+//	}
+//}
+//
+//func isConnected(x, y int) bool {
+//	return parent[x] == parent[y]
+//}
+//
+//func initFindUnion(n int) {
+//	parent = make([]int, n)
+//	for i := 0; i < n; i++ {
+//		parent[i] = i
+//	}
+//}
